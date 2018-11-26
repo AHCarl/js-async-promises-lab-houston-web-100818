c828f4cbda17e73d397ec96ee767e9cd731d41c5
Alexander Carlin <ahcarlin@gmail.com>
2018-11-26 01:25:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,6 +41,6 @@ function trueAndFalseButtons() {
 
 function toggleTrueAndFalseButtons() {
   for (let btn of trueAndFalseButtons()) {
-    btn.querySelector
+    btn.querySelectorAll("btn")
   }
 }
